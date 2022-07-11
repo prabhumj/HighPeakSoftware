58f8dcaad4de0a772a63037cffb3db2b711f6421
Prabhu <45420938+prabhumj@users.noreply.github.com>
2022-07-11 16:50:31 +0530

    Add files via upload

---
+++ Item.java
@@ -0,0 +1,15 @@
+
+class Item {
+	String name;
+	int price;
+
+	public Item(String name, int price) {
+		this.name = name;
+		this.price = price;
+	}
+
+	public String toString() { 
+		return this.name + ": " + this.price;
+	}
+}
+
